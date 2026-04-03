ae7748a08d55ab9f6be0864dad49a9440719a86c
martinsmeder <119725728+martinsmeder@users.noreply.github.com>
2026-04-03 08:07:30 +0200

    Renamed file, added comments

--- hello-world.ts
+++ hello-world.ts
@@ -2,8 +2,10 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 
+// Create server.
 const server = new McpServer({ name: "hello-server", version: "1.0.0" });
 
+// Add tool.
 server.registerTool(
   "say_hello",
   { description: "Says hello", inputSchema: { name: z.string() } },
@@ -12,6 +14,8 @@ server.registerTool(
   }),
 );
 
+// Use stdin (standard input) and stdout (standard output) for communication
+// between client and server. Then, connect to the server.
 const transport = new StdioServerTransport();
 await server.connect(transport);
 console.error("Server running...");
